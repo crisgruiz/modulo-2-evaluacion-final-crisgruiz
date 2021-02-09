b8cd4d5e6c07f28b7789dcd69837cb4c48e18dc6
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-02-09 12:38:33 +0100

    Add favorite functions

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@
 const inputElement = document.querySelector(".js-input");
 const btnElement = document.querySelector(".js-searchBtn");
 const listElement = document.querySelector(".js-list");
+const favElement = document.querySelector(".js-favContainer");
 
 let shows = [];
+let favorites = [];
 
 function handleGetToApi() {
   const inputValue = inputElement.value.toLowerCase();
@@ -16,7 +18,7 @@ function handleGetToApi() {
 }
 function paintSeries() {
   let htmlCode = "";
-  // const inputValue = inputElement.value.toLowerCase();
+  const inputValue = inputElement.value.toLowerCase();
 
   for (const show of shows) {
     const showName = show.name.toLowerCase();
@@ -24,15 +26,16 @@ function paintSeries() {
     //Creamos una variable vacía y la igualamos a la función que llama la imagen
     let showImage = GetImagesDefault(show);
 
-    // if (showName.includes(inputValue)) {
-    htmlCode += `<li class="previewSerie js-series">`;
-    htmlCode += `<h3 class="previewSerie__title js-serieTitle">${show.name}</h3>`;
-    htmlCode += `<img class="previewSerie__img js-serieImg" src="${showImage}" class="">`;
-    htmlCode += `</li>`;
-    // }
+    if (showName.includes(inputValue)) {
+      htmlCode += `<li class="previewSerie js-series" id="${show.id}">`;
+      htmlCode += `<h3 class="previewSerie__title js-serieTitle">${show.name}</h3>`;
+      htmlCode += `<img class="previewSerie__img js-serieImg" src="${showImage}" class="">`;
+      htmlCode += `</li>`;
+    }
   }
   listElement.innerHTML = htmlCode;
   listenSeriesEvent();
+  // paintFavorites();
 }
 
 //Función que nos permite llamar a la imagen
@@ -41,6 +44,13 @@ function GetImagesDefault(show) {
     return show.image.medium;
   } else {
     return `https://via.placeholder.com/210x295/ffffff/666666/?text=${show.name}`;
+  }
+}
+function GetImagesFav(favorite) {
+  if (favorite.image) {
+    return favorite.image.medium;
+  } else {
+    return `https://via.placeholder.com/210x295/ffffff/666666/?text=${favorite.name}`;
   }
 }
 
@@ -53,8 +63,41 @@ function listenSeriesEvent() {
   }
 }
 
-function handleFavorite() {
-  console.log("Me han seleccionado");
+//Función que añade lo seleccionado al array Favorites
+
+function handleFavorite(ev) {
+  const click = parseInt(ev.currentTarget.id);
+  const indexFav = favorites.findIndex(function listenSeriesEvent(show) {
+    return show.id === click;
+  });
+  const isFavorite = indexFav !== -1;
+  if (isFavorite === true) {
+    favorites.splice(indexFav, 1);
+  } else {
+    for (let i = 0; i < shows.length; i++) {
+      if (shows[i].id === click) {
+        favorites.push(shows[i]);
+      }
+    }
+  }
+  paintFavorites();
+}
+
+//Pintar favoritos
+
+function paintFavorites() {
+  let htmlCode = "";
+  htmlCode += `<ul class = "favSerie">`;
+  for (const favorite of favorites) {
+    let favoriteImage = GetImagesFav(favorite);
+    console.log("me están pintando");
+    htmlCode += `<li class="favSerie-list js-favSeries" id="${favorite.id}">`;
+    htmlCode += `<h3 class="favSerie-list__title js-favSerieTitle">${favorite.name}</h3>`;
+    htmlCode += `<img class="favSerie-list__img js-favSerieImg" src="${favoriteImage}" class="">`;
+    htmlCode += `</li>`;
+  }
+  htmlCode += `</ul>`;
+  favElement.innerHTML = htmlCode;
 }
 
 btnElement.addEventListener("click", handleGetToApi);
