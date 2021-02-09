64250ed5ed4c7b18b75a079ba29b292a009335aa
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-02-09 10:22:31 +0100

    Clean code

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 const inputElement = document.querySelector(".js-input");
 const btnElement = document.querySelector(".js-searchBtn");
 const listElement = document.querySelector(".js-list");
+
 let shows = [];
 
 function handleGetToApi() {
@@ -15,27 +16,45 @@ function handleGetToApi() {
 }
 function paintSeries() {
   let htmlCode = "";
-  const inputValue = inputElement.value.toLowerCase();
+  // const inputValue = inputElement.value.toLowerCase();
+
   for (const show of shows) {
-    console.log(show);
     const showName = show.name.toLowerCase();
 
-    //Creamos una variable vacía y con la condición le damos valor
-    let showImage = "";
-    if (show.image) {
-      showImage = show.image.medium;
-    } else {
-      showImage = `https://via.placeholder.com/210x295/ffffff/666666/?text=${show.name}`;
-    }
-    if (showName.includes(inputValue)) {
-      htmlCode += `<li class="previewSerie">`;
-      htmlCode += `<h3 class="previewSerie__title js-serieTitle">${show.name}</h3>`;
-      htmlCode += `<img class="previewSerie js-serieImg" src="${showImage}" class="">`;
+    //Creamos una variable vacía y la igualamos a la función que llama la imagen
+    let showImage = GetImagesDefault(show);
 
-      htmlCode += `</li>`;
-    }
+    // if (showName.includes(inputValue)) {
+    htmlCode += `<li class="previewSerie js-series">`;
+    htmlCode += `<h3 class="previewSerie__title js-serieTitle">${show.name}</h3>`;
+    htmlCode += `<img class="previewSerie__img js-serieImg" src="${showImage}" class="">`;
+    htmlCode += `</li>`;
+    // }
   }
   listElement.innerHTML = htmlCode;
+  listenSeriesEvent();
+}
+
+//Función que nos permite llamar a la imagen
+function GetImagesDefault(show) {
+  if (show.image) {
+    return show.image.medium;
+  } else {
+    return `https://via.placeholder.com/210x295/ffffff/666666/?text=${show.name}`;
+  }
+}
+
+//Selección de favoritos
+
+function listenSeriesEvent() {
+  const serieElements = document.querySelectorAll(".js-series");
+  for (const serieElement of serieElements) {
+    serieElement.addEventListener("click", handleFavorite);
+  }
+}
+
+function handleFavorite() {
+  console.log("Me han seleccionado");
 }
 
 btnElement.addEventListener("click", handleGetToApi);
